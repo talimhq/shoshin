1d83f263eccbe018ead6a45fc3f37d9e78ddd6e3
Igor de Alcântara Barroso <idabmat@gmail.com>
2016-09-28 12:47:07 -0300

    fix js lint issues

--- app/assets/javascripts/chat.js
+++ app/assets/javascripts/chat.js
@@ -7,7 +7,7 @@ var scrollToBottom = function() {
 
 var parseMessagesAuthor = function() {
   $('.message').each(function(index) {
-    if ($(this).data('user-type') != $(this).prev().data('user-type') & $(this).data('user-id') != $(this).prev().data('user-id')) {
+    if ($(this).data('user-type') !== $(this).prev().data('user-type') & $(this).data('user-id') !== $(this).prev().data('user-id')) {
       $(this).addClass('first');
     }
   });
@@ -17,7 +17,7 @@ document.addEventListener('turbolinks:load', function() {
   parseMessagesAuthor();
   scrollToBottom();
   $('#new_group_notification').on('keypress', function(e) {
-    if (e && e.keyCode == 13) {
+    if (e && e.keyCode === 13) {
       e.preventDefault();
       $(this).submit();
     }
